d7ab652dc493e4549c7def8065a9701c6f1f2c6f
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-11-30 23:22:47 +0100

    -

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -3,48 +3,48 @@ import QuizCollection from './QuizCollection';
 import { useEffect } from 'react';
 import QuizComponent from './QuizComponent';
 export default function UserPage() {
-    const [userName, setUserName] = useState("Wilgot");
-    const [allQuizComponents, setAllQuizComponents] = useState([]);
+  const [userName, setUserName] = useState("Wilgot");
+  const [allQuizComponents, setAllQuizComponents] = useState([]);
 
-    useEffect(() => {
-      fetchMyQuizComponents();
-    }, [])
-  
-    async function fetchMyQuizComponents(){
-      try {
-        const response = await fetch("https://localhost:7283/api/Quiz/getMyQuizzes", {
-          method: 'GET',
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem("AccessToken")}`
-          }
-        })
-  
-        const data = await response.json();
-        if (response.status === 200) {
-          console.log(data);
-          setAllQuizComponents(data);
-        } else {
-          alert(data);
+  useEffect(() => {
+    fetchMyQuizComponents();
+  }, [])
+
+  async function fetchMyQuizComponents() {
+    try {
+      const response = await fetch("https://localhost:7283/api/Quiz/getMyQuizzes", {
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem("AccessToken")}`
         }
-  
-      } catch (err) {
-        console.log(err.toString())
+      })
+
+      const data = await response.json();
+      if (response.status === 200) {
+        console.log(data);
+        setAllQuizComponents(data);
+      } else {
+        alert(data);
       }
+
+    } catch (err) {
+      console.log(err.toString())
     }
+  }
 
 
   return (
-    <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
-      <div style={{width: "80%", height: "fit-content", background: "#DEECFC", boxShadow: "0px 2px 2px 1px rgba(0, 0, 0, 0.2)", padding: "50px"}}>
-        <div style={{display: "flex", flexDirection: "column"}}>
-          <div style={{display: "flex", width: "100%"}}>  
-            <div style={{width: "350px", height: "350px", background: "orange", display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <div style={{ width: "80%", height: "fit-content", background: "#DEECFC", boxShadow: "0px 2px 2px 1px rgba(0, 0, 0, 0.2)", padding: "50px" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ display: "flex", width: "100%" }}>
+            <div style={{ width: "350px", height: "350px", background: "orange", display: "flex", justifyContent: "center", alignItems: "center" }}>
               <h1>Uername</h1>
             </div>
-            <div style={{width: "1000px", display: "flex", justifyContent: "left", paddingLeft: "20px"}}>
+            <div style={{ width: "1000px", display: "flex", justifyContent: "left", paddingLeft: "20px" }}>
               <div>
                 <p className='description-text underline'><b>Joined:</b> 18 months ago</p>
                 <p className='description-text underline'><b>Quizes:</b> 7</p>
@@ -54,8 +54,8 @@ export default function UserPage() {
           </div>
           <div>
             <h1>Quizes:</h1>
-            <div style={{width:"100%", display:"flex"}}>
-            {allQuizComponents.map(quiz => <div style={{width:"200px"}}><QuizComponent quizName = {quiz.quizName} /></div>)}
+            <div style={{display:"grid", gridTemplateColumns:"repeat(3,1fr)", gap:"40px"}} >
+              {allQuizComponents.map(quiz => <QuizComponent quizName={quiz.quizName} />)}
             </div>
           </div>
         </div>
