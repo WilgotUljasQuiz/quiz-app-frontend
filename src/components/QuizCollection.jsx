ab2c50f9278b0b638d6892f75dc309c703906e86
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-11-30 22:48:24 +0100

    -

--- src/components/QuizCollection.jsx
+++ src/components/QuizCollection.jsx
@@ -2,26 +2,39 @@ import React, { useEffect, useState } from 'react'
 import QuizComponent from './QuizComponent'
 
 export default function QuizCollection() {
-    const [allQuizComponents, setAllQuizComponents] = useState([]);
+  const [allQuizComponents, setAllQuizComponents] = useState([]);
 
-    useEffect(() => {
-        //fetch all quizes components from api
-    }, [])
+  useEffect(() => {
+    fetchQuizComponents();
+  }, [])
+
+  async function fetchQuizComponents(){
+    try {
+      const response = await fetch("https://localhost:7283/api/Quiz/getQuizzes", {
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+
+      const data = await response.json();
+      if (response.status === 200) {
+        console.log(data);
+        setAllQuizComponents(data);
+      } else {
+        alert(data);
+      }
+
+    } catch (err) {
+      console.log(err.toString())
+    }
+  }
+
   return (
     <div className='quiz-grid'>
-        {allQuizComponents.map(quiz => <QuizComponent id={quiz.id}/>)}
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
-        <QuizComponent />
+      {allQuizComponents.map(quiz => <QuizComponent quizName = {quiz.quizName} />)}
     </div>
   )
 }
--- src/components/QuizComponent.jsx
+++ src/components/QuizComponent.jsx
@@ -2,21 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 
-export default function QuizComponent({id}) {
-    const [title, setTitle] = useState("[Title]")
-    const [cathegory, setCathegory] = useState("not defined");
-    const [quiestionsCount, setQuestionsCount] = useState(0);
-    const [difficulty, setDifficulty] = useState("not defined")
-
-    const [questions, setQuestions] = useState([]);
-
+export default function QuizComponent(props) {
     const navigate = useNavigate();
     const navigatePath = (path) => navigate(`/${path}`);
     
-    useEffect(() => {
-        //get all data from API
-    }, [])
-
     function openQuiz(){
 
         //open quiz with id
@@ -24,10 +13,10 @@ export default function QuizComponent({id}) {
     }
   return (
     <div style={{width: "300px", height: "400px", background: "blue"}} onClick={() => navigatePath("quiz")}>
-        <h1>{title}</h1>
-        <p>Cathegory: {cathegory}</p>
-        <p>Difficulty: {difficulty}</p>
-        <p>Questions: {quiestionsCount}</p>
+        <h1>{props.quizName}</h1>
+        <p>Cathegory: </p>
+        <p>Difficulty: </p>
+        <p>Questions: </p>
     </div>
   )
 }
