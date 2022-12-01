ebf5b04723758d074780415d6011871b5892ea9d
Wilgot <wilgotwardell@hotmail.com>
2022-12-01 21:10:59 +0100

    -

--- src/components/PlayPage.jsx
+++ src/components/PlayPage.jsx
@@ -12,37 +12,32 @@ function PlayPage() {
   const [activeQuestion, setActiveQuestion] = useState([]);
 
   
+  const [test, setTest] = useState(false);
 
   useEffect(() => {
-    console.log(params.quizId);
+    // console.log(params.quizId);
     getQuestionIds();
   }, [])
 
   async function getQuestionIds(){
-    // try{
-      const response = await fetch("https://localhost:7283/api/Quiz/getQuestionIds?quizid=" + params.quizId, {
-        method: 'GET',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      })
-      console.log(await response.json());
-
-      const data = await response.json();
-      setQuestionIds(await data);
-    // }catch(err){
-    //   console.log(err);
-    // }
+    const response = await fetch("https://localhost:7283/api/Quiz/getQuestionIds?quizid=" + params.quizId, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    const data = await response.json();
+    setQuestionIds(await data);
   }
 
   
 
   async function getActiveQuestion(){
-    console.log(questionIds[0]);
-    // try{
-      const response = await fetch("https://localhost:7283/api/Quiz/getQuestion?questionId=" + questionIds[0], {
+    
+    try{
+      const response = await fetch("https://localhost:7283/api/Quiz/getQuestion?questionId=" + questionIds[activeCount], {
         method: 'GET',
         headers: {
           'Access-Control-Allow-Origin': '*',
@@ -50,15 +45,20 @@ function PlayPage() {
           'Accept': 'application/json'
         }
       })
-      // console.log(await response.json())
       const data = await response.json();
-      // setActiveQuestion(await data);
-      console.log(await data)
-    // }catch(err){
-    //   console.log(err);
-    // }
-    // console.log(activeQuestion);
-    // setActiveCount(prev => prev + 1);
+      console.log(await data);
+
+      setActiveQuestion(await data);
+
+      setTest(true);
+      console.log(activeQuestion);
+      setActiveCount(prev => prev + 1);
+
+      console.log(activeQuestion.length);
+    }catch(err){
+      console.log(err);
+    }
+    
   }
   return (
     <div>
@@ -71,11 +71,11 @@ function PlayPage() {
             <h1>{activeQuestion.title}</h1>
             <div style={{display: "flex", flexDirection: "column"}}>
               {/* answers: */}
-              {/* {activeQuestion != undefined &&
+              {test && 
                 <>
-                  {activeQuestion.answers.map(answer => <p>{answer.title}</p>)}
+                  {activeQuestion.answers.map(answer => <p key={Math.random() * 1000}>{answer.title}</p>)}
                 </>
-              } */}
+              }
             </div>
           </div>
           <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
